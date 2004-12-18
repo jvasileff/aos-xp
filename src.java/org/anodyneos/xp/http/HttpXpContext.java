6f0fe2ef28ce87a334e1f8fbd04e8a7d434f0d49
Yao Xiong <yxiong@marketingcentral.com>
2004-12-18 00:15:21 +0000

    Fleshed out some of the features

--- src.java/org/anodyneos/xp/http/HttpXpContext.java
+++ src.java/org/anodyneos/xp/http/HttpXpContext.java
@@ -7,7 +7,9 @@
 package org.anodyneos.xp.http;
 
 import org.anodyneos.xp.XpContext;
-
+import java.util.HashMap;
+import javax.servlet.jsp.el.ExpressionEvaluator;
+import javax.servlet.jsp.el.VariableResolver;
 
 /**
  * @author jvas
@@ -17,14 +19,64 @@ import org.anodyneos.xp.XpContext;
  */
 public abstract class HttpXpContext implements XpContext {
 
-    public static final int PAGE_SCOPE = 0;
-    public static final int REQUEST_SCOPE = 1;
-    public static final int SESSION_SCOPE = 2;
-    public static final int APPLICATION_SCOPE = 3;
+    private ExpressionEvaluator expressionEvaluator;
+    private VariableResolver  variableResolver;
 
-    public static final String PAGE_SCOPE_STRING = "page";
-    public static final String REQUEST_SCOPE_STRING = "request";
-    public static final String SESSION_SCOPE_STRING = "session";
-    public static final String APPLICATION_SCOPE_STRING = "application";
+    public static enum Scope {
+            PAGE_SCOPE(0,"page"),
+            REQUEST_SCOPE(1,"request"),
+            SESSION_SCOPE(2,"session"),
+            APPLICATION_SCOPE(3,"application");
 
+            private int intValue;
+            private String strValue;
+
+            Scope (int intValue, String strValue){
+                this.intValue = intValue;
+                this.strValue = strValue;
+            }
+            public int toInt() {return intValue;}
+            public String toString() {return strValue;}
+        };
+
+    public static HashMap<String,Scope> scopeMap = new HashMap<String,Scope>();
+    static{
+        scopeMap.put(Scope.PAGE_SCOPE.toString(),Scope.PAGE_SCOPE);
+        scopeMap.put(Scope.REQUEST_SCOPE.toString(),Scope.REQUEST_SCOPE);
+        scopeMap.put(Scope.SESSION_SCOPE.toString(),Scope.SESSION_SCOPE);
+        scopeMap.put(Scope.APPLICATION_SCOPE.toString(),Scope.APPLICATION_SCOPE);
+    }
+
+    public int resolveScope(String scope){
+        if (scopeMap.containsKey(scope)){
+            Scope scopeInstance = scopeMap.get(scope);
+            return scopeInstance.toInt();
+        }else{
+            return getDefaultScope();
+        }
+    }
+
+    public Scope resolveToScope(String scope){
+        return scopeMap.get(scope);
+    }
+
+    public int getDefaultScope(){
+        return Scope.PAGE_SCOPE.toInt();
+    }
+    public void setExpressionEvaluator(ExpressionEvaluator expressionEvaluator){
+        this.expressionEvaluator = expressionEvaluator;
+    }
+    public void setVariableResolver(VariableResolver  variableResolver ){
+        this.variableResolver = variableResolver;
+    }
+    public ExpressionEvaluator getExpressionEvaluator(){
+        return expressionEvaluator;
+    }
+    public VariableResolver getVariableResolver(){
+        return variableResolver;
+    }
+
+    public Object getAttribute(String name){return getAttribute(name,getDefaultScope());}
+    public void setAttribute(String name, Object obj){setAttribute(name, obj, getDefaultScope());}
+    public void removeAttribute(String name){removeAttribute(name,getDefaultScope());}
 }
