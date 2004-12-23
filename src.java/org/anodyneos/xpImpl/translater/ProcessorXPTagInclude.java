769dd7465464f83fc06a69dd903970a8346bf4e4
Yao Xiong <yxiong@marketingcentral.com>
2004-12-23 17:21:11 +0000

    no longer instantiating included xp pages in current class loader, but using caching class loader to load from class loader specific to xp page

--- src.java/org/anodyneos/xpImpl/translater/ProcessorXPTagInclude.java
+++ src.java/org/anodyneos/xpImpl/translater/ProcessorXPTagInclude.java
@@ -7,7 +7,9 @@ import org.xml.sax.Attributes;
 import org.xml.sax.SAXException;
 import org.xml.sax.SAXParseException;
 import java.util.HashMap;
-
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
 /**
  * @author yao
  */
@@ -44,9 +46,54 @@ public class ProcessorXPTagInclude extends TranslaterProcessor {
             throw new SAXParseException("@file is a required attribute for <include>", getContext().getLocator());
         }
 
-        out.printIndent().println("new " +
-                TranslaterContext.DEFAULT_PACKAGE + "." + file.replace('/','.') + "().service(xpContext,xpCH);");
+        getTranslaterContext().addDependent(file);
+        file = stripExtension(file);
+        if (file.startsWith("/") || file.startsWith("\\")){
+            file = file.substring(1,file.length());
+        }
+        //out.printIndent().println("new " +
+        //        TranslaterContext.DEFAULT_PACKAGE + "." + file.replace('/','.') + "().service(xpContext,xpCH);");
+        out.printIndent().println("getIncludedPage(\"" + TranslaterContext.DEFAULT_PACKAGE + "." + file.replace('/','.')+
+                                                    "\").service(xpContext,xpCH);");
+    }
+    private static String stripExtension(String fileName){
+        int dotXp = fileName.indexOf(".xp");
+        if (dotXp != -1){
+            return fileName.substring(0,dotXp);
+        }else{
+            return fileName;
+        }
+    }
+    public static String getContextRelativePath(ServletRequest request,
+            String relativePath) {
 
+        if (relativePath.startsWith("/")){
+            return (relativePath);
+        }
+
+        if (!(request instanceof HttpServletRequest)){
+            return (relativePath);
+        }
+        HttpServletRequest hrequest = (HttpServletRequest) request;
+        String uri = (String)
+        request.getAttribute("javax.servlet.include.servlet_path");
+        if (uri != null) {
+            String pathInfo = (String)
+            request.getAttribute("javax.servlet.include.path_info");
+            if (pathInfo == null) {
+                if (uri.lastIndexOf('/') >= 0){
+                    uri = uri.substring(0, uri.lastIndexOf('/'));
+                }
+            }
+        }
+        else {
+            uri = hrequest.getServletPath();
+            if (uri.lastIndexOf('/') >= 0)
+            {
+                uri = uri.substring(0, uri.lastIndexOf('/'));
+            }
+        }
+        return uri + '/' + relativePath;
     }
 
     public void characters(char[] ch, int start, int length) throws SAXException {
