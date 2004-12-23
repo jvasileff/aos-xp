cfb7dae5d0acb5ce058bdc3fef592d07ffd8f395
Yao Xiong <yxiong@marketingcentral.com>
2004-12-23 17:18:02 +0000

    dependents allow determination at runtime of wether or not to reload a page

--- src.java/org/anodyneos/xp/XpPage.java
+++ src.java/org/anodyneos/xp/XpPage.java
@@ -3,9 +3,11 @@ package org.anodyneos.xp;
 import javax.servlet.jsp.el.ELException;
 
 import org.xml.sax.SAXException;
+import java.util.List;
 
 public interface XpPage {
 
+    public List getDependents();
     public abstract void service(XpContext xpContext, XpContentHandler out)
     throws XpException, ELException, SAXException;
 
