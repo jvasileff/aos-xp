3a2fe7e8b67f046a30afbc2826b1cc124adc24ca
Yao Xiong <yxiong@marketingcentral.com>
2005-01-07 20:27:21 +0000

    Changed to be more intelligent

--- src.java/org/anodyneos/xpImpl/runtime/exception/XpTranslationException.java
+++ src.java/org/anodyneos/xpImpl/runtime/exception/XpTranslationException.java
@@ -1,22 +1,59 @@
 package org.anodyneos.xpImpl.runtime.exception;
 
+import java.io.FileNotFoundException;
+
+import org.xml.sax.SAXParseException;
+
 public class XpTranslationException extends XpRuntimeException{
     /**
      * Comment for <code>serialVersionUID</code>
      */
     private static final long serialVersionUID = 3834032437427713081L;
 
-    long lineNumber;
+    long lineNumber = -1;
+    long colNumber = -1;
+    String fileName;
+    String internalMessage;
 
     public XpTranslationException(Throwable rootCause) {
         super(rootCause);
-        // TODO Auto-generated constructor stub
+        // if it's a SAXParseException, let's take whatever info we can from it
+        if (rootCause instanceof SAXParseException) {
+
+            SAXParseException saxException = (SAXParseException) rootCause;
+            setLineNumber(saxException.getLineNumber());
+            setColNumber(saxException.getColumnNumber());
+            setFileName(saxException.getSystemId());
+
+            // generate an informative/useful error message
+            StringBuffer retVal = new StringBuffer("XpTranslationException: ");
+            retVal.append(rootCause.getMessage());
+            retVal.append("  File: ");
+            retVal.append(getFileName());
+            retVal.append(" Line: ");
+            retVal.append(getLineNumber());
+            retVal.append(" Column: ");
+            retVal.append(getColNumber());
+            setInternalMessage(retVal.toString());
+        } else {
+            setInternalMessage(rootCause.getMessage());
+        }
     }
 
     public XpTranslationException(String message) {
         super(message);
     }
 
+    public String getMessage(){
+
+        String myMessage = getInternalMessage();
+
+        if (myMessage == null) {
+            myMessage = super.getMessage();
+        }
+
+        return myMessage;
+    }
 
     /**
      * @return Returns the lineNumber.
@@ -29,5 +66,41 @@ public class XpTranslationException extends XpRuntimeException{
      */
     public void setLineNumber(long lineNumber) {
         this.lineNumber = lineNumber;
+    }
+    /**
+     * @return Returns the colNumber.
+     */
+    public long getColNumber() {
+        return colNumber;
+    }
+    /**
+     * @param colNumber The colNumber to set.
+     */
+    public void setColNumber(long colNumber) {
+        this.colNumber = colNumber;
+    }
+    /**
+     * @return Returns the fileName.
+     */
+    public String getFileName() {
+        return fileName;
+    }
+    /**
+     * @param fileName The fileName to set.
+     */
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+    /**
+     * @return Returns the internalMessage.
+     */
+    public String getInternalMessage() {
+        return internalMessage;
+    }
+    /**
+     * @param internalMessage The internalMessage to set.
+     */
+    private void setInternalMessage(String internalMessage) {
+        this.internalMessage = internalMessage;
     }
 }
